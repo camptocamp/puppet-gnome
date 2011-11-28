6413a6e682a663af08f52e3e2e2ea2e27d2949fc
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2011-11-28 13:51:03 +0100

    gnome: new module for gnome configuration

---
+++ manifests/gsettings.pp
@@ -0,0 +1,50 @@
+/*
+
+== Definition: gnome::gsettings
+
+Sets a configuration key in Gnome's GSettings registry.
+
+Parameters:
+- *$user*: the name of the system user (see user param of 'exec')
+- *$schema*: the GSettings Schema to address
+- *$key*: the key whose value will be changed
+- *$value_type*: the type of the $value you give (defaults to 'String')
+- *$value*: the value to set
+- *$list_append*: wether the GSettings key is an array and the value you set
+  should be appended to the existing array (defaults to 'false')
+
+
+Notes:
+
+Never add quotes to $value.
+
+If the GSettings key is an Array and you set $list_append to false, the $value
+must be in the form of a matching GVariant serialized value (e.g. for arrays
+of strings (type 'as'): "['value1', 'value2']").
+
+If you want to add a String value to an array of strings, set $list_append to
+"true", $value_type to "String" (default). $value must only contain the string
+you want to append without quotes.
+
+*/
+define gnome::gsettings($user, $schema, $key, $value_type="String", $value, $list_append=false) {
+
+  case $value_type {
+    'String': { $prep_value = "'${value}'" }
+    /Integer|Boolean|Array/: { $prep_value = "${value}" }
+    default: { fail "Invalid type '${type}'" }
+  }
+
+  $command = $list_append ? {
+    # I think this commands only work when there is and X session running
+    true  => "DISPLAY=:0 gsettings set ${schema} ${key} \"`gsettings get ${schema} ${key} | sed s/.$//`, ${prep_value}]\"",
+    false => "DISPLAY=:0 gsettings set ${schema} ${key} \"${prep_value}\"",
+  }
+
+  exec {"set ${key} on user ${user} to ${prep_value}":
+    command => "${command}",
+    unless  => "gsettings get ${schema} ${key} | grep -q \"${prep_value}\"",
+    user    => "$user",
+  }
+
+}
