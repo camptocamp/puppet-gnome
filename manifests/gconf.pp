5420796a816be181a75fd03ecdb26b77293418ef
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-12 10:18:10 +0100

    Linting

--- manifests/gconf.pp
+++ manifests/gconf.pp
@@ -3,9 +3,9 @@ define gnome::gconf(
   $keyname,
   $type,
   $value,
-  $list_type=false,
-  $list_append=false,
-  $schema='',
+  $list_type=undef,
+  $list_append=undef,
+  $schema=undef,
   $ensure='present',
 ) {
   if $list_type {
@@ -14,24 +14,24 @@ define gnome::gconf(
       $unless  = "su -l -c '/usr/bin/gconftool-2 --get ${keyname}' ${user} | grep ${value}"
     }
     else {
-      $command = "su -l -c \"/usr/bin/gconftool-2 --set $keyname --list-type $list_type --type $type '$value'\" $user"
-      $unless  = "test \"$value\" != \"\" && su -l -c 'test \"$(/usr/bin/gconftool-2 --get $keyname)\" == \"$value\"' $user"
+      $command = "su -l -c \"/usr/bin/gconftool-2 --set ${keyname} --list-type ${list_type} --type ${type} '${value}'\" ${user}"
+      $unless  = "test \"${value}\" != \"\" && su -l -c 'test \"$(/usr/bin/gconftool-2 --get ${keyname})\" == \"${value}\"' ${user}"
     }
   }
   else {
-    $command = "su -l -c \"/usr/bin/gconftool-2 --set $keyname --type $type '$value'\" $user"
-    $unless  = "test \"$value\" != \"\" && su -l -c 'test \"$(/usr/bin/gconftool-2 --get $keyname)\" == \"$value\"' $user"
+    $command = "su -l -c \"/usr/bin/gconftool-2 --set ${keyname} --type ${type} '${value}'\" ${user}"
+    $unless  = "test \"${value}\" != \"\" && su -l -c 'test \"$(/usr/bin/gconftool-2 --get ${keyname})\" == \"${value}\"' ${user}"
   }
-  exec {"set param $keyname with value $value for user $user":
+  exec {"set param ${keyname} with value ${value} for user ${user}":
     command => $command,
     unless  => $unless,
     require => User[$user],
   }
   if $schema {
-    exec {"set default schema entry for $keyname in $user":
-      command => "su -l -c 'gconftool-2 --apply-schema $schema $keyname' ${user}",
+    exec {"set default schema entry for ${keyname} in ${user}":
+      command => "su -l -c 'gconftool-2 --apply-schema ${schema} ${keyname}' ${user}",
       unless  => "test \"${schema}\" == \"$(su -l -c '/usr/bin/gconftool-2 --get-schema-name ${keyname}' ${user})\"",
-      require => Exec["set param $keyname with value $value for user $user"],
+      require => Exec["set param ${keyname} with value ${value} for user ${user}"],
     }
   }
 }
