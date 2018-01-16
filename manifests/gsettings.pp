325768b067623dd43b613263e89079e1a712595f
Adam Butler <Adam.J.Butler@gmail.com>
2018-01-16 10:04:09 +0000

    Add ability to run gsettings for a specific user (#23)

--- manifests/gsettings.pp
+++ manifests/gsettings.pp
@@ -8,13 +8,22 @@ define gnome::gsettings(
   $value,
   $directory = '/usr/share/glib-2.0/schemas',
   $priority  = '25',
+  $user = undef,
 ) {
-  file { "${directory}/${priority}_${name}.gschema.override":
-    content => "[${schema}]\n  ${key} = ${value}\n",
-  }
+  if ($user == undef) {
+    file { "${directory}/${priority}_${name}.gschema.override":
+      content => "[${schema}]\n  ${key} = ${value}\n",
+    }
 
-  ~> exec { "change-${schema}-${key}":
-    command     => "/usr/bin/glib-compile-schemas ${directory}",
-    refreshonly => true,
+    ~> exec { "change-${schema}-${key}":
+      command     => "/usr/bin/glib-compile-schemas ${directory}",
+      refreshonly => true,
+    }
+  } else {
+    exec { "change-${schema}-${key}":
+      command => "dbus-launch gsettings set ${schema} ${key} ${value}",
+      path    => '/usr/bin',
+      user    => $user,
+    }
   }
 }
